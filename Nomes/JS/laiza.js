48f91f5f396754cb0bc43e7420ef3e845a3aaef1
Joao-santos-oliveira <80595573+Joao-santos-oliveira@users.noreply.github.com>
2022-08-18 22:20:43 -0300

    opa

---
+++ Nomes/JS/laiza.js
@@ -0,0 +1,9 @@
+const senha = 3131
+const senhainerit = prompt ("Digite sua senha")
+
+if (senha == senhainerit){
+    document.getElementById("index.html").style.display= "none"
+}
+else if (senha != senhainerit){
+    window.location.href = 'erro.html'
+}
